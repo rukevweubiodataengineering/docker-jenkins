6be87c3f5f5d42027fcefec0c47f613226145b3f
rukevweubiodataengineering <ubiowororuki@gmail.com>
2025-08-25 19:25:54 +0100

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,6 @@
 // tests/app.test.js
 const request = require('supertest');
-const app = require('../Node.js/server');
+const app = require('../nodejs/server');
 
 describe('API Endpoints', () => {
   describe('GET /', () => {
